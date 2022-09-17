81fbf70911377100f55237c527713051a5632e9d
Alessio Sferro <sferro.alessio@gmail.com>
2022-09-17 09:35:01 +0200

    feat: add player movement, add player jump animation

--- main.js
+++ main.js
@@ -3,30 +3,165 @@ const ctx = canvas.getContext("2d");
 const jump = document.querySelector("#jump");
 const idle = document.querySelector("#idle");
 
-// ctx.drawImage(image, 96, 0, 32, 32, 20, 20, 32, 32);
+let player = {
+  x: canvas.width / 2 - 16,
+  y: canvas.height / 2 - 16,
+  size: 32,
+  dx: 0,
+  dy: 0,
+  speed: 1.75,
+  animation: idle,
+  currentAnimationFrame: 0,
+  totalAnimationFrames: 4,
+  isAnimationLooping: true,
+};
 
-let i = 0;
+function updatePlayerAnimationFrame() {
+  setInterval(() => {
+    updatePlayer({
+      currentAnimationFrame: player.isAnimationLooping
+        ? (player.currentAnimationFrame + 1) % player.totalAnimationFrames
+        : player.currentAnimationFrame + 1,
+    });
 
-setInterval(() => {
-  i = (i + 1) % 4;
-}, 100);
+    if (
+      !player.isAnimationLooping &&
+      player.currentAnimationFrame === player.totalAnimationFrames
+    ) {
+      updatePlayer({
+        animation: idle,
+        currentAnimationFrame: 0,
+        isAnimationLooping: true,
+        totalAnimationFrames: 4,
+      });
+    }
+  }, 150);
+}
 
-async function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function playerJump() {
+  updatePlayer({
+    animation: jump,
+    currentAnimationFrame: 0,
+    isAnimationLooping: false,
+    totalAnimationFrames: 8,
+  });
+}
 
+function updatePlayer(update) {
+  player = {
+    ...player,
+    ...update,
+  };
+}
+
+function getHorizontalPlayerPosition() {
+  // right side collision detection
+  if (player.x + player.size + player.dx > canvas.width) {
+    return player.x;
+  }
+
+  // left side collision detection
+  if (player.x + player.dx < 0) {
+    return 0;
+  }
+
+  return player.x + player.dx;
+}
+
+function getVerticalPlayerPosition() {
+  if (player.y + player.size + player.dy > canvas.height) {
+    return player.y;
+  }
+
+  if (player.y + player.dy < 0) {
+    return 0;
+  }
+
+  return player.y + player.dy;
+}
+
+function drawPlayer() {
   ctx.drawImage(
-    idle,
-    i * 32,
+    player.animation,
+    player.currentAnimationFrame * 32,
     0,
     32,
     32,
-    canvas.width / 2 - 16,
-    canvas.height / 2 - 16,
+    player.x,
+    player.y,
     32,
     32
   );
+}
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  updatePlayer({
+    x: getHorizontalPlayerPosition(),
+    y: getVerticalPlayerPosition(),
+  });
+
+  drawPlayer();
 
   requestAnimationFrame(update);
 }
 
+function movePlayer() {
+  document.addEventListener("keydown", (event) => {
+    if (event.key !== "ArrowUp" || player.dy < 0) return;
+
+    updatePlayer({ dy: player.dy - player.speed });
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key !== "ArrowRight" || player.dx > 0) return;
+
+    updatePlayer({ dx: player.dx + player.speed });
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key !== "ArrowDown" || player.dy > 0) return;
+
+    updatePlayer({ dy: player.dy + player.speed });
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key !== "ArrowLeft" || player.dx < 0) return;
+
+    updatePlayer({ dx: player.dx - player.speed });
+  });
+
+  document.addEventListener("keyup", (event) => {
+    if (event.key !== "ArrowUp") return;
+
+    updatePlayer({ dy: player.dy + player.speed });
+  });
+
+  document.addEventListener("keyup", (event) => {
+    if (event.key !== "ArrowRight") return;
+
+    updatePlayer({ dx: player.dx - player.speed });
+  });
+
+  document.addEventListener("keyup", (event) => {
+    if (event.key !== "ArrowDown") return;
+
+    updatePlayer({ dy: player.dy - player.speed });
+  });
+
+  document.addEventListener("keyup", (event) => {
+    if (event.key !== "ArrowLeft") return;
+
+    updatePlayer({ dx: player.dx + player.speed });
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key !== " ") return;
+    playerJump();
+  });
+}
+
 update();
+updatePlayerAnimationFrame();
+movePlayer();
